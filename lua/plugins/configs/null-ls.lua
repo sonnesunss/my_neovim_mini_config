cad3fc9a34680a656366cbef9d420474c016ad83
sun <145575806+sonnesunss@users.noreply.github.com>
2025-04-16 20:54:29 +0800

    first
    相较于原版添加修改了如下内容:
    
    1.  add tokyonight theme to builtin_plugins
    2.  add set guifont -> Source Code Pro/h12
    3.  add Lsp rust_analyer
    4.  add treesitter_server rust
    5.  load rust lsp default

---
+++ lua/plugins/configs/null-ls.lua
@@ -0,0 +1,29 @@
+--
+-- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+-- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+-- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+-- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+-- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+-- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+--
+-- File: plugins/configs/null-ls.lua
+-- Description: null-ls configuration
+-- Original-Author: Kien Nguyen-Tuan <kiennt2609@gmail.com>
+local null_ls = require("null-ls")
+-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
+-- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null_ls.builtins.diagnostics
+
+-- Load custom configurations
+local exist, custom = pcall(require, "custom")
+local sources = exist
+    and type(custom) == "table"
+    and custom.setup_sources
+    and custom.setup_sources(null_ls.builtins)
+    or {}
+
+null_ls.setup({
+    debug = false,
+    sources = sources
+})
